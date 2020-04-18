596495a892e013ddf66d7202d9b2281ab5f5ad6b
ZoraizQ <21100130@lums.edu.pk>
2020-04-18 06:46:13 +0500

    fix: remove extra reducer error
    Remove the store/index.js error preventing the app from starting

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import taskDataReducer from '../features/task-management/taskdataSlice'
-import requestListReducer from '../features/request-management/requestListSlice'
 
 export default configureStore({
   reducer: {
     taskData: taskDataReducer,
-    requestListData: requestListReducer
   },
 })
