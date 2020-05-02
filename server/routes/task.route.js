b65bb3e29f7b1063f89df78d96be75efdbd34ce3
Hammad Nasir <hammadn99@gmail.com>
2020-05-02 12:07:30 +0500

    add: create route and validation for task management api
    #65

---
+++ server/routes/task.route.js
@@ -0,0 +1,134 @@
+'use strict'
+/*
+  ------------------ DEPENDENCIES --------------------
+*/
+
+// Others:
+var router = require('express').Router();
+var validate = require('express-validation').validate;
+var jwt = require('../services/jwt');
+var taskValidation = require('../validations/task.validation');
+var taskController = require('../controllers/submission.controller');
+var { validateUserAccess, validateCCAAccess } = require('../services/access-validator');
+
+/*
+  ------------------ CODE BODY --------------------
+*/
+
+// API 5.1: Create Request Task
+router.post(
+  '/task/req/create',
+  validate(taskValidation.createReqTaskValidation, { keyByField: true }),
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.2: Create Custom Task
+router.post(
+  '/task/cus/create',
+  validate(taskValidation.createCusTaskValidation, { keyByField: true }),
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.3: Edit Request Task
+router.post(
+  '/task/req/edit',
+  validate(taskValidation.editReqTaskValidation, { keyByField: true }),
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.4: Edit Custom Task
+router.post(
+  '/task/cus/edit',
+  validate(taskValidation.editCusTaskValidation, { keyByField: true }),
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.5: Add Log
+router.post(
+  '/log/add',
+  validate(taskValidation.addLogValidation, { keyByField: true }),
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.6: Fetch Task Manager
+router.post(
+  '/fetch',
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.7: Fetch Archive Manager
+router.post(
+  '/fetch-archive',
+  // Validation Missing!
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.8: Fetch Task
+router.post(
+  '/task/fetch',
+  validate(taskValidation.fetchTaskValidation, { keyByField: true }),
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.9: Create Task Status
+router.post(
+  '/task-status/create',
+  validate(taskValidation.createTaskStatusValidation, { keyByField: true }),
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.10: Edit Task Status
+router.post(
+  '/task-status/edit',
+  validate(taskValidation.editTaskStatusValidation, { keyByField: true }),
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.11: Delete Task Status
+router.post(
+  '/task-status/delete',
+  validate(taskValidation.deleteTaskStatusValidation, { keyByField: true }),
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
+
+// API 5.12: Create Task Status
+router.post(
+  '/task-status/fetch-all',
+  jwt.verify,
+  validateUserAccess,
+  validateCCAAccess,
+  // Controller Missing!
+);
---
+++ server/validations/task.validation.js
@@ -0,0 +1,114 @@
+'use strict'
+/*
+  ------------------ DEPENDENCIES --------------------
+*/
+
+// Modules:
+var Joi = require('express-validation').Joi;
+
+/*
+  ------------------ CODE BODY --------------------
+*/
+
+// Export API 5.1 Validation Object:
+exports.createReqTaskValidation = {
+  body: Joi.object({
+    task: Joi.object({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      submissionId: Joi.number().required(),
+      ownerId: Joi.number().required(),
+      statusId: Joi.number().required(),
+      checklistIds: Joi.array().items(Joi.object({
+        checklistId: Joi.number().required(),
+        assigneeId: Joi.number().required()
+      }))
+    }).required()
+  })
+}
+
+// Export API 5.2 Validation Object:
+exports.createCusTaskValidation = {
+  body: Joi.object({
+    task: Joi.object({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      ownerId: Joi.number().required(),
+      statusId: Joi.number().required(),
+    }).required()
+  })
+}
+
+// Export API 5.3 Validation Object:
+exports.editReqTaskValidation = {
+  body: Joi.object({
+    task: Joi.object({
+      taskId: Joi.string().required(),
+      title: Joi.string(),
+      description: Joi.string(),
+      ownerId: Joi.number(),
+      statusId: Joi.number(),
+      subtasks: Joi.array().items(Joi.object({
+        subtaskId: Joi.number().required(),
+        assigneeId: Joi.number(),
+        description: Joi.string(),
+        check: Joi.bool()
+      }))
+    })
+  })
+}
+
+// Export API 5.4 Validation Object:
+exports.editCusTaskValidation = {
+  body: Joi.object({
+    task: Joi.object({
+      taskId: Joi.string().required(),
+      title: Joi.string(),
+      description: Joi.string(),
+      ownerId: Joi.number(),
+      statusId: Joi.number(),
+    }).required()
+  })
+}
+
+// Export API 5.5 Validation Object:
+exports.addLogValidation = {
+  body: Joi.object({
+    taskId: Joi.string().required(),
+    description: Joi.string().required()
+  })
+}
+
+// Export API 5.7 Validation Object:
+// MISSING!
+
+// Export API 5.8 Validation Object:
+exports.fetchTaskValidation = {
+  body: Joi.object({
+    taskId: Joi.string().required()
+  })
+}
+
+// Export API 5.9 Validation Object:
+exports.createTaskStatusValidation = {
+  body: Joi.object({
+    name: Joi.string().required(),
+    color: Joi.color().required()
+  })
+}
+
+// Export API 5.10 Validation Object:
+exports.editTaskStatusValidation = {
+  body: Joi.object({
+    statusId: Joi.number().required(),
+    name: Joi.string(),
+    color: Joi.color()
+  })
+}
+
+// Export API 5.11 Validation Object:
+exports.deleteTaskStatusValidation = {
+  body: Joi.object({
+    statusId: Joi.number().required(),
+  })
+}
