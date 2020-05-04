1b1dc39e406a451a78556b42b5414fad88864a3c
ZoraizQ <21100130@lums.edu.pk>
2020-05-04 06:25:45 +0500

    style: submission list styling and timestamp helper

--- client/src/features/request-management/request-list/RequestList.js
+++ client/src/features/request-management/request-list/RequestList.js
@@ -153,7 +153,7 @@ export function RequestList({requestListData, dispatch}) {
               />
             </DialogContent>
             <DialogActions>
-              <Button onClick={handleDialogClose} color="primary" autoFocus>
+              <Button onClick={handleDialogClose} color="primary">
                 Save
               </Button>
             </DialogActions>
@@ -183,7 +183,7 @@ export function RequestList({requestListData, dispatch}) {
           </MuiPickersUtilsProvider>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleDateDialogClose} color="primary" autoFocus>
+          <Button onClick={handleDateDialogClose} color="primary" autoFocus type="submit">
             Save
           </Button>
         </DialogActions>
--- client/src/helpers.js
+++ client/src/helpers.js
@@ -9,35 +9,48 @@ API Caller helper to refactor common API code that requires bearer tokens (all h
 */
 
 export async function apiCaller(api, body, successCode, dataReturner, rejectWithValue) {
-    try {
-        let req_init = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${localStorage.token}`, 
-            },
-        }
+  try {
+    let req_init = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.token}`, 
+      },
+    }
 
-        // if body is an empty object, do not include it
-        if (!(Object.keys(body).length === 0 && body.constructor === Object)){
-            req_init['body'] = JSON.stringify(body)
-        }
-                
-        const res = await fetch(api, req_init)
-        if (res.ok) {
-            const data = await res.json()
-            console.log(data)
-            if (data.statusCode != successCode) {
-                throw new Error((data.error !== undefined) 
-                ? `${data.statusCode}: ${data.message} - ${JSON.stringify(data.error.details).replace(/[\[\]\{\}"'\\]+/g, '').split(':').pop()}`
-                : `${data.statusCode}: ${data.message}`) 
-            }
-            return dataReturner(data)
-        }
-        throw new Error(`${res.status}, ${res.statusText}`) 
+    // if body is an empty object, do not include it
+    if (!(Object.keys(body).length === 0 && body.constructor === Object)){
+      req_init['body'] = JSON.stringify(body)
     }
-    catch (err) {
-        return rejectWithValue(err.toString())
+    
+    const res = await fetch(api, req_init)
+    if (res.ok) {
+      const data = await res.json()
+      console.log(data)
+      if (data.statusCode != successCode) {
+        throw new Error((data.error !== undefined) 
+        ? `${data.statusCode}: ${data.message} - ${JSON.stringify(data.error.details).replace(/[\[\]\{\}"'\\]+/g, '').split(':').pop()}`
+        : `${data.statusCode}: ${data.message}`) 
+      }
+      return dataReturner(data)
     }
+    throw new Error(`${res.status}, ${res.statusText}`) 
+  }
+  catch (err) {
+    return rejectWithValue(err.toString())
+  }
 }
+
+export function simplifyTimestamp(ts, dateOnly=true) {
+  const monthAbbrev = {"01":"Jan", "02":"Feb", "03":"Mar", "04":"Apr", "05":"May", "06":"Jun", "07":"Jul", "08":"Aug",
+  "09":"Sep", "10":"Oct", "11":"Nov", "12":"Dec"}
+  const dateTime = ts.split('T')
+  const YYMMDD = dateTime[0].split('-')
+  
+  if (dateOnly) {
+    return (`${YYMMDD[2]} ${monthAbbrev[YYMMDD[1]]} ${YYMMDD[0]}`)
+  }
+} 
+
+simplifyTimestamp("2020-05-02T20:57:09.891Z")
